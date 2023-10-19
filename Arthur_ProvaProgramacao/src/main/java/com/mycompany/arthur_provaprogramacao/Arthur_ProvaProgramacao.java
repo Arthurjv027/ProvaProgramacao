85eec0025b926cedaa5afbe73e9ab9d83d67fc06
Arthurjv027 <thuthujotave@gmail.com>
2023-10-19 19:01:03 -0300

    teste

---
+++ Arthur_ProvaProgramacao/src/main/java/com/mycompany/arthur_provaprogramacao/Arthur_ProvaProgramacao.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arthur_provaprogramacao;
+
+/**
+ *
+ * @author aoliveira
+ */
+public class Arthur_ProvaProgramacao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
